dba1fb1c5813cfecbe7f7d4e6b83c6475d0371c1
Taylor Thurman <taylor.thurman@protonmail.com>
2023-11-13 17:06:05 -0600

    initial commit lol + Sidebar + Document list + Archive Functionality

---
+++ convex/documents.ts
@@ -0,0 +1,95 @@
+import {v} from "convex/values";
+import {mutation, query} from "./_generated/server";
+import {Id} from "./_generated/dataModel";
+
+export const archive = mutation({
+  args: { id: v.id("documents") },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new Error("Not authenticated");
+    }
+
+    const userId = identity.subject;
+    const existingDocument = await ctx.db.get(args.id);
+
+    if (!existingDocument) {
+      throw new Error("Document not found");
+    }
+
+    if (existingDocument.userId !== userId) {
+      throw new Error("Not authorized");
+    }
+
+    const recursiveArchive = async (documentId: Id<"documents">) => {
+      const children = await ctx.db.query("documents")
+        .withIndex("by_user_parent", (q) => (
+          q.eq("userId", userId).eq("parentDocument", documentId)
+        )).collect();
+
+      for (const child of children) {
+        await ctx.db.patch(child._id, {
+          isArchived: true
+        });
+
+        await recursiveArchive(child._id);
+      }
+    }
+
+    const document = await ctx.db.patch(args.id, {
+      isArchived: true
+    });
+
+    await recursiveArchive(args.id);
+
+    return document;
+  }
+});
+export const getSidebar = query({
+  args: {
+    parentDocument: v.optional(v.id("documents"))
+  },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new Error("Not authenticated");
+    }
+
+    const userId = identity.subject;
+
+    return ctx.db.query("documents")
+      .withIndex("by_user_parent", (q) =>
+        q.eq("userId", userId)
+          .eq("parentDocument", args.parentDocument)
+      )
+      .filter((q) => q.eq(q.field("isArchived"), false))
+      .order("desc")
+      .collect();
+  }
+});
+
+export const create = mutation({
+  args: {
+    title: v.string(),
+    parentDocument: v.optional(v.id("documents"))
+  },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new Error("Not authenticated");
+    }
+
+    const userId = identity.subject;
+
+    return await ctx.db.insert("documents", {
+      title: args.title,
+      parentDocument: args.parentDocument,
+      userId: userId,
+      isArchived: false,
+      isPublished: false
+    });
+  }
+});
